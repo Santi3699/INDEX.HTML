5ca3ab1dfcbfe91027642380682d6d4c03fcaefe
Santi3699 <santiagopicon19992@gmail.com>
2025-04-23 17:29:54 -0500

    Create variables230425.js

---
+++ INDEX.HTML 08/variables230425.js
@@ -0,0 +1,13 @@
+let valor1 = 6
+let valor2 = 15
+
+let suma = valor1 * valor2
+let residuo = valor1 % valor2
+//valor1 = valor1 + valor 2
+
+valor1 += valor2
+
+
+console.log(valor1)
+console.log(suma)
+console.log(residuo)
