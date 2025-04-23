39d52a1686a9bc6c86bd304bc68653ed1121fa4a
Santi3699 <santiagopicon19992@gmail.com>
2025-04-23 17:21:38 -0500

    Update listas230425.js

--- listas230425.js
+++ listas230425.js
@@ -1,1 +1,14 @@
+lista = ["juan", "pedro", "carlos"]
 
+objeto = {"nombre": "carlos", "profesion":"ingeniero", "telefono":"3158423452", "email":"peju@gmail.com" }
+
+console.log(lista[0])
+console.log(lista[1])
+console.log(lista[2])
+console.log(lista[3])
+
+
+console.log(objeto["nombre"])
+console.log(objeto["profesion"])
+console.log(objeto["telefono"])
+console.log(objeto["email"])
