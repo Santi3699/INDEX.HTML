1521fb4ec5fc1e6b7fa0e6767a4fc69877a37f19
Santi3699 <santiagopicon19992@gmail.com>
2025-04-24 17:39:46 -0500

    Create condicionales240425.js

---
+++ condicionales240425.js
@@ -0,0 +1,9 @@
+const entrada = document.getElementById("entrada")
+const entrada2 = document.getElementById("entrada2")
+const resultados = document.getElementById("resultados")
+
+function ejecutar(){
+    let valor1 = parseFloat(entrada.value)
+    let valor2 = parseFloat(entrada2.value)
+    let suma = valor1 + valor2
+}
